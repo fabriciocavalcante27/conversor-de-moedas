3ec9e93583df94edef21189126b669ea323f046b
Fabricio Silva Cavalcante <126496672+fabriciocavalcante27@users.noreply.github.com>
2023-06-06 13:18:12 -0300

    esse é o meu decimo primeiro commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues() {
+   const inputCurrencyValue = document.querySelector(".input-currency").value
+
+   const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+   const currencyValueConverted = document.querySelector(".currency-value")
+
+   const dolarToday = 4.98
+
+   const convertValue = inputCurrencyValue / dolarToday
+
+   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+   }).format(inputCurrencyValue)
+   currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+   }).format(convertValue)
+}
+convertButton.addEventListener("click", convertValues)
